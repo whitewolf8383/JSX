bd1655a26a4385b808012f2baa8f24f1bd4d35fe
whitewolf8383 <kandt.vandf@gmail.com>
2021-08-24 14:15:11 -0600

    react setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <div>
-    <h1>Favorite Foods</h1>
-    <ul>
-      <li>Curry</li>
-      <li>Hotdog</li>
-      <li>Sushi</li>
-    </ul>
-  </div>,
-  document.getElementById('root'));
+const name = "MyName";
+
+ReactDOM.render(<h1>Hello {name}</h1>,document.getElementById('root'));
